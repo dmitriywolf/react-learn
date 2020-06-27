88c06b161f13fbdc98bbb3c6097954147d2bd9ad
Dmitriy Volchenko <dmitriywolf@users.noreply.github.com>
2020-06-27 21:30:26 +0300

    start create toDo list

--- src/index.js
+++ src/index.js
@@ -4,14 +4,22 @@ import ReactDOM  from 'react-dom';
 
 const root = document.getElementById('root');
 
+//JSX
+const element = (
+  <div>
+    <h1>My TODO list</h1>
 
-//const element = <h1>First Element</h1>;
+    <input placeholder="search"/>
 
-const element = React.createElement(
-  'h1',
-  null,
-  'First Element'
+  </div>
 );
+
+//Обычный код без JSX
+// const element = React.createElement(
+//   'h1',
+//   null,
+//   'First Element'
+// );
 
 
 
