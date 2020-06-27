623d897dc12d29a07d6c0538f80aa5ce46085398
Dmitriy Volchenko <dmitriywolf@users.noreply.github.com>
2020-06-27 22:22:50 +0300

    update index

--- src/index.js
+++ src/index.js
@@ -1,30 +1,61 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
+import { Simulate } from 'react-dom/test-utils';
 
-
+//Корневой элемент
 const root = document.getElementById('root');
 
-//JSX
-const element = (
-  <div>
+
+//Функциональный компонент TodoList 
+const TodoList = () => {
+
+  const items = ['Learn React','Create awesome app'];
+
+  return (
+    <ul>
+      <li>{ items[0] }</li>
+      <li>{ items[1] }</li>
+    </ul>
+  );
+};
+
+//Функциональный компонент AppHeader
+const AppHeader = () => {
+  return (
     <h1>My TODO list</h1>
+  );
+};
 
-    <input placeholder="search"/>
+//Коспонент поисковая панель
+const SearchPanel = () => {
+  const searchText = 'Type here to search';
 
+  const searchStyle = {
+    fontSize: '20px'
+  }
+
+  return (
+    <input 
+    style = {searchStyle}
+    placeholder={searchText}/>
+  );
+};
+
+
+//Главный компонент App
+const App = () => {
+  return (
+    <div>
+    <AppHeader/>
+    <SearchPanel/>
+    <TodoList/>
   </div>
-);
-
-//Обычный код без JSX
-// const element = React.createElement(
-//   'h1',
-//   null,
-//   'First Element'
-// );
-
+  );
+};
 
 
 ReactDOM.render(
-  element,
+  <App/>,
   root
 );
 
