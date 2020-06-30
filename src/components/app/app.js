53e356bc50096a9fbb61976614b6a515f9b66859
Dmitriy Volchenko <dmitriywolf@users.noreply.github.com>
2020-06-30 14:52:08 +0300

    del elements

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel'
@@ -7,29 +7,54 @@ import ItemStatusFilter from '../item-status-filter';
 
 import './app.css';
 
-const App = () => {
 
-    const todoData = [
+
+export default class App extends Component {
+
+  state = {
+    todoData: [
       {id: 1, label: 'Drink Coffe', important: true},
       {id: 2, label: 'Drink tea', important: false},
       {id: 3, label: 'Drink milk', important: true},
       {id: 4, label: 'Drink water', important: false},
-    ];
-    
-      return (
-        <div className="todo-app">
-        <AppHeader toDo={1} done={3}/>
-        <div className="top-panel d-flex">
-          <SearchPanel/>
-          <ItemStatusFilter/>
-        </div>
-        
-        <TodoList 
-          todos = {todoData}
-          onDeleted = { (id) => console.log('del', id) }
-          />
-      </div>
-      );
-    };
+      {id: 5, label: 'Drink vodka', important: false},
+      {id: 6, label: 'Drink bear', important: false},
+    ]
+  };
 
-    export default App;
+  deleteItem = (id) => {
+    this.setState( ( { todoData } ) => {
+
+      const idx = todoData.findIndex( (el) => el.id === id );
+      
+      const newArray = [
+        ... todoData.slice( 0, idx ),
+        ... todoData.slice( idx + 1 )
+      ];
+
+      return {
+        todoData: newArray
+      };
+
+    });
+  };
+    
+  render() {
+    return (
+      <div className="todo-app">
+      <AppHeader toDo={1} done={3}/>
+      <div className="top-panel d-flex">
+        <SearchPanel/>
+        <ItemStatusFilter/>
+      </div>
+      
+      <TodoList 
+        todos = {this.state.todoData}
+        onDeleted = { this.deleteItem }
+        />
+    </div>
+    );
+  };
+  
+
+}
