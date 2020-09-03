dc59da169e466c2a1bdbe2aefd49a06ac0fefab2
dmitriywolf <dmitriyvolchenko3003@gmail.com>
2020-09-03 20:44:55 +0300

    update header component

--- toDoList/src/components/header/header.js
+++ toDoList/src/components/header/header.js
@@ -5,9 +5,18 @@ import React from 'react';
 import styled from "styled-components";
 
 const AppHeader = styled.div`
-  display: flex
+  display: flex;
   align-items: flex-end;
   justify-content: space-between;
+
+  h1 {
+    font-size: 26px;
+  }
+
+  h2 {
+    font-size: 1.2rem;
+    color: grey;
+  }
 `
 
 const Header = () => {
