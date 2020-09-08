71d9fa6c1401c39261205d3ee9423011263edbe1
dmitriywolf <dmitriyvolchenko3003@gmail.com>
2020-09-08 19:13:42 +0300

    add event adding items

--- toDoList/src/components/post-add-form/post-add-form.js
+++ toDoList/src/components/post-add-form/post-add-form.js
@@ -1,25 +1,39 @@
-import React from "react";
+import React, {Component} from "react";
 import "./post-add-form.css";
 
+export default class PostAddForm extends Component {
+  constructor(props){
+    super(props);
+    this.onValueChange = this.onValueChange.bind(this);
 
 
-const PostAddForm = ({onAddItem}) => {
-  return ( 
-    <div className="bottom-panel d-flex">
-      <input
-        type="text"
-        placeholder="Напишите свою следующую задачу"
-        className="form-control from-new-post-label"
-      />
-      <button
-        type="submit"
-        className="btn btn-outline-secondary"
-        onClick={ () => onAddItem("hello Item") }>
-        Добавить</button>
-    </div>
-  );
+  }
 
+
+  onValueChange(e){
+    console.log(e.target.value)
+  }
+
+
+  render(){
+    const {onAddItem} = this.props;
+
+    return (
+      <form className="bottom-panel d-flex">
+        <input
+          type="text"
+          placeholder="Напишите свою следующую задачу"
+          className="form-control from-new-post-label"
+          onChange={this.onValueChange}
+        />
+        <button
+          type="submit"
+          className="btn btn-outline-secondary"
+          onClick={ () => onAddItem("hello Item") }>
+          Добавить
+        </button>
+      </form>
+    )
+  }
 
 }
-
-export default PostAddForm;
