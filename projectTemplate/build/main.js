184ee7ab3babf1296507373d09a19cbd7931d487
dmitriywolf <dmitriyvolchenko3003@gmail.com>
2020-09-21 19:25:51 +0300

    add webpack

--- projectTemplate/build/main.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const App = () => /*#__PURE__*/React.createElement("h1", null, "Hello world");
-
-ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById('root'));
