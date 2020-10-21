f8f402c3a930b4bc223845ad2f85dab815b73fad
dmitriywolf <dmitriyvolchenko3003@gmail.com>
2020-10-21 18:57:24 +0300

    fetch

--- starDB/src/index.js
+++ starDB/src/index.js
@@ -1,11 +1,25 @@
 
-const getResource = async (url) => {  
-  const res = await fetch(url);
-  const body = await res.json();
-  return body;
-};
+// fetch("https://swapi.dev/api/people/1/")
+//   .then( (response) => {
+//     console.log('Responce status: ',  response.status);
+//     return response.json();
+//   })
+//   .then( (body) => {
+//     console.log(body);
+//   })
 
-getResource('https://swapi.dev/api/people/1/')
-  .then( (body) => {
-    console.log(body)
-  });
+
+const getResource = async (url) => {
+
+  const response = await fetch(url);
+
+  const body = await response.json();
+
+  return body;
+}
+
+getResource("https://swapi.dev/api/people/1/")
+  .then( (result) => {
+    console.log(result);
+  })
+  .catch( error => console.log(error))
