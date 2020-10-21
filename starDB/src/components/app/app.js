45ac5b41f03193d732061a290fcd7513c69a9cf9
dmitriywolf <dmitriyvolchenko3003@gmail.com>
2020-10-21 21:39:25 +0300

    add component

---
+++ starDB/src/components/app/app.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import AppHeader from '../app-header';
+import RandomPlanet from '../random-planet';
+import ItemList from '../item-list';
+import PersonDetails from '../person-details';
+
+import './app.css';
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <RandomPlanet />
+
+      <div className="row mb2">
+        <div className="col-md-6">
+          <ItemList />
+        </div>
+        <div className="col-md-6">
+          <PersonDetails />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- starDB/src/index.js
+++ starDB/src/index.js
@@ -1,64 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// fetch("https://swapi.dev/api/people/1/")
-//   .then( (response) => {
-//     console.log('Responce status: ',  response.status);
-//     return response.json();
-//   })
-//   .then( (body) => {
-//     console.log(body);
-//   })
+
+import SwapiService from './services/swapi-service'
+import App from './components/app';
 
 
 
-
-class SwapiService {
-
-  _apiBase = 'https://swapi.dev/api';
-
-  async getResource(url){
-    const response = await fetch(`${this._apiBase}${url}`);
-
-    if(!response.ok) {
-      throw new Error(`Could not fetch ${url}, received ${response.status}`)
-    }
-    return  await response.json();
-  }
-
-
-
-
-
-  async getAllPeople() {
-    const res = await this.getResource(`/people/`);
-    return res.results;
-  }
-
-  getPerson(id) {
-    return this.getResource(`/people/${id}/`)
-  }
-
-
-  async getAllPlanets() {
-    const res = await this.getResource(`/planets/`);
-    return res.results;
-  }
-
-  getPlanet(id) {
-    return this.getResource(`/planets/${id}/`)
-  }
-
-
-  async getAllStarships() {
-    const res = await this.getResource(`/starships/`);
-    return res.results;
-  }
-
-  getStarships(id) {
-    return this.getResource(`/starships/${id}/`)
-  }
-
-}
-
+ReactDOM.render (
+  <App/>,
+  document.querySelector('#root')
+)
 
 const swapi = new SwapiService();
 swapi.getPerson(3)
