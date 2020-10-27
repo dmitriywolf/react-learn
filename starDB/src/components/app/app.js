9041d5e2b5c4c07ea0ff1201232bf8331e2f7cc4
dmitriywolf <dmitriyvolchenko3003@gmail.com>
2020-10-27 21:23:44 +0200

    update app

--- starDB/src/components/app/app.js
+++ starDB/src/components/app/app.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ErrorBoundry from '../error-boundry';
-
-import ItemDetails, { Record } from "../item-details/item-details";
 import SwapiService from "../../services/swapi-service";
-import DummySwapiService from "../../services/dummy-swapi-service";
 import { SwapiServiceProvider } from "../swapi-service-context";
+import Row from '../row';
 
 import {
   PersonDetails,
@@ -21,57 +18,14 @@ import {
 import './app.css';
 
 export default class App extends Component {
-
   swapiService = new SwapiService();
 
   state = {
     showRandomPlanet: true
   };
 
-  toggleRandomPlanet = () => {
-    this.setState((state) => {
-      return {
-        showRandomPlanet: !state.showRandomPlanet
-      }
-    });
-  };
-
   render() {
-
-    const planet = this.state.showRandomPlanet ?
-      <RandomPlanet/> :
-      null;
-
-    const { getPerson,
-            getStarship,
-            getPersonImage,
-            getStarshipImage,
-            getAllPeople,
-            getAllPlanets } = this.swapiService;
-
-    const personDetails = (
-      <ItemDetails
-        itemId={11}
-        getData={getPerson}
-        getImageUrl={getPersonImage} >
-
-        <Record field="gender" label="Gender" />
-        <Record field="eyeColor" label="Eye Color" />
-
-      </ItemDetails>
-    );
-
-    const starshipDetails = (
-      <ItemDetails
-        itemId={5}
-        getData={getStarship}
-        getImageUrl={getStarshipImage}>
-
-        <Record field="model" label="Model" />
-        <Record field="length" label="Length" />
-        <Record field="costInCredits" label="Cost" />
-      </ItemDetails>
-    );
+    const planet = this.state.showRandomPlanet ? <RandomPlanet/> : null;
 
     return (
       <ErrorBoundry>
@@ -79,18 +33,12 @@ export default class App extends Component {
           <div className="stardb-app">
             <Header />
 
-            <PersonDetails itemId={11} />
+            {planet}
 
-            <PlanetDetails itemId={5} />
-
-            <StarshipDetails itemId={9} />
-
-            <PersonList />
-
-            <StarshipList />
-
-            <PlanetList />
-
+            <Row left={<PersonList/>} right={<PersonDetails itemId={20}/>} />
+            <Row left={<PlanetList/>} right={<PlanetDetails itemId={5}/>} />
+            <Row left={<StarshipList/>} right={<StarshipDetails itemId={9}/>} />
+            
           </div>
         </SwapiServiceProvider>
       </ErrorBoundry>
---
+++ starDB/src/components/pages/people-page.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import { PersonDetails, PersonList } from '../sw-components';
+
+export default class PeoplePage extends Component {
+
+  state = {
+    selectedItem: null
+  };
+
+  render() {
+
+  };
+};
