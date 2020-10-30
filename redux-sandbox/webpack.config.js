a04222ee1079dff20b51fdea32713fb545fef010
dmitriywolf <dmitriyvolchenko3003@gmail.com>
2020-10-30 13:33:48 +0200

    start redux app

---
+++ redux-sandbox/webpack.config.js
@@ -0,0 +1,98 @@
+const path = require('path');
+const HtmlWepackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = ( env = {} ) =>  {
+
+  // Определяем значение перменной
+  const {mode = 'development'} = env;
+  const isProd = mode === 'production';
+  const isDev = mode === 'development';
+
+  const getStyleLoaders = () => {
+    return [
+      isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+      'css-loader'
+    ];
+  };
+
+  const getPlugins = () => {
+    const plugins = [
+      new HtmlWepackPlugin({
+        template: 'public/index.html'
+      })
+    ];
+
+    if(isProd) {
+      plugins.push(new MiniCssExtractPlugin({
+        filename: 'main-[hash:8].css'
+      }))
+    }
+
+    return plugins;
+  };
+
+  return {
+    mode: isProd ? 'production' : isDev && 'development',
+    entry: './src/index.js',
+    output: {
+      filename: isProd ? 'bundle-[hash:8].js' : undefined,
+      path: path.resolve(__dirname, 'dist'),
+      // publicPath: '/'
+    },
+  
+  
+    module: {
+      rules: [
+        // Babel-loader
+        { 
+          test: /\.(js|jsx)$/, 
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+  
+        // Loading Images
+       {
+         test: /\.(png|jpg|jpeg|gif|ico)$/,
+         use: [{
+            loader: 'file-loader',
+            options: {
+              outputPath: 'img',
+              name: '[name]-[sha1:hash:7].[ext]'
+            }
+          }]
+        },
+        // Loading Fonts
+        {
+          test: /\.(ttf|otf|eot|woff|woff2)$/,
+          use: [{
+             loader: 'file-loader',
+             options: {
+               outputPath: 'fonts',
+               name: '[name].[ext]'
+             }
+           }]
+         },
+  
+         // Loading CSS
+         {
+          test: /\.(css)$/,
+          use: getStyleLoaders()
+         },
+  
+         // Loading SCSS/SASS
+         {
+          test: /\.(s[ca]ss)$/,
+          use: [ ...getStyleLoaders(), 'sass-loader']
+         }
+      ]
+    },
+  
+    plugins: getPlugins(),
+  
+    devServer: {
+      open: true,   // open in browser
+      historyApiFallback: true
+    }
+  };
+};
